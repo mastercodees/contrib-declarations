848edde8954619fcc8520fd6a8126f0b169704aa
Martin Ratinaud <martinratinaud@gmail.com>
2022-08-23 14:59:53 +0400

    WIP fix Instagram privacy policy with filters
    This should maybe be done with multi page instead

--- declarations/Instagram.filters.js
+++ declarations/Instagram.filters.js
@@ -1,3 +1,5 @@
+import puppeteer from 'puppeteer';
+
 export const replaceInfoImageWithEmoji = document => {
   const infoImages = document.querySelectorAll('img[src*="/851547_537948159656190_540847388_n.png?"]');
 
@@ -16,4 +18,40 @@ export function removeTrackingIDs(document) {
       el.setAttribute('href', params.toString());
     }
   });
+}
+
+export async function getModalContent(document, { fetch }) {
+  const subpageLinks = document.querySelectorAll('a[href*="subpage="]');
+  const annotationLinks = document.querySelectorAll('a[href*="annotations["]');
+
+  let browser;
+
+  try {
+    browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    await page.goto(fetch);
+
+    for (const link of [ ...annotationLinks, ...subpageLinks ]) {
+      console.log(link.href);
+      const linkSelector = `a[href="${link.href.replace('https://privacycenter.instagram.com/', '/')}"]`;
+
+      await page.waitForSelector(linkSelector);
+      await page.click(linkSelector);
+
+      const contentSelector = "[role='dialog'] [role='main']";
+
+      await page.waitForSelector(contentSelector);
+      const mainContent = await page.$(contentSelector);
+
+      const newItem = document.createElement('div');
+
+      newItem.innerHTML = `<hr/>${(await mainContent.getProperty('innerHTML')).toString()}<hr/>`;
+      link.parentNode.replaceChild(newItem, link);
+      await page.click('[role="dialog"] [aria-label="Close"]');
+    }
+  } catch (e) {
+    await browser.close();
+    throw e;
+  }
 }
